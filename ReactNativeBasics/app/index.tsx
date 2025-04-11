682bada3c49bc425766ad2f1fc02496903da0e65
Amandeep Singh <programmeramandeep@gmail.com>
2025-04-11 21:23:30 +0530

    feat: Implement goal input functionality and improve layout

--- ReactNativeBasics/app/index.tsx
+++ ReactNativeBasics/app/index.tsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
+	const [enteredGoalText, setEnteredGoalText] = useState<string>("");
+
+	const goalInputHandler = (enteredText: string) => {
+		setEnteredGoalText(enteredText);
+	};
+
+	const addGoalHandler = () => {
+		console.log(enteredGoalText);
+		setEnteredGoalText("");
+	};
+
 	return (
 		<View style={styles.appContainer}>
 			<View style={styles.inputContainer}>
-				<TextInput placeholder="Your course goal!" style={styles.textInput} />
-				<Button title="Add Goal" />
+				<TextInput placeholder="Your course goal!" style={styles.textInput} onChangeText={goalInputHandler} />
+				<Button title="Add Goal" onPress={addGoalHandler} />
 			</View>
-			<View>
+			<View style={styles.goalsContainer}>
 				<Text>List of goals...</Text>
 			</View>
 		</View>
@@ -16,17 +28,27 @@ export default function Index() {
 
 const styles = StyleSheet.create({
 	appContainer: {
-		padding: 50,
+		flex: 1,
+		paddingTop: 24,
+		paddingHorizontal: 16,
 	},
 	inputContainer: {
 		flexDirection: "row",
 		justifyContent: "space-between",
+		alignItems: "center",
+		paddingBottom: 24,
+		marginBottom: 24,
+		borderBottomWidth: 1,
+		borderBottomColor: "#ccc",
 	},
 	textInput: {
 		borderWidth: 1,
 		borderColor: "#ccc",
-		width: "80%",
+		width: "70%",
 		marginRight: 9,
 		padding: 8,
+	},
+	goalsContainer: {
+		flex: 5,
 	},
 });
