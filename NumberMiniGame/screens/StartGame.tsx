4ae29d8ce407cc2a5ae7ea7dc68aa94eda45d7eb
Amandeep Singh <programmeramandeep@gmail.com>
2025-04-13 21:54:15 +0530

    feat: Styling for Android & iOS

--- NumberMiniGame/screens/StartGame.tsx
+++ NumberMiniGame/screens/StartGame.tsx
@@ -1,13 +1,28 @@
 import PrimaryButton from "@/components/PrimaryButton";
 import React from "react";
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 
 export default function StartGame() {
 	return (
-		<View>
+		<View style={styles.inputContainer}>
 			<TextInput placeholder="Enter your number" placeholderTextColor={"#ccc"} />
 			<PrimaryButton>Reset</PrimaryButton>
 			<PrimaryButton>Confirm</PrimaryButton>
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	inputContainer: {
+		padding: 16,
+		marginTop: 100,
+		marginHorizontal: 24,
+		backgroundColor: "#72063c",
+		borderRadius: 8,
+		elevation: 4,
+		shadowColor: "#000",
+		shadowOffset: { width: 0, height: 2 },
+		shadowRadius: 6,
+		shadowOpacity: 0.25,
+	},
+});
